742a1214d97e146b12edd2547ee9e03648268de2
mazakh2011 <mazakh1705@yandex.ru>
2021-07-18 16:10:36 +0300

    New changes

--- test_product_page.py
+++ test_product_page.py
@@ -72,3 +72,12 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+	
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    basket_page = BasketPage(browser, link)
+    basket_page.go_to_basket()
+    basket_page.basket_should_be_empty()
